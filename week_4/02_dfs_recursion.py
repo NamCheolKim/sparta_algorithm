f4d1a0273f486214a86643391291f71c964de591
NamCheolKim <pkeowkd@naver.com>
2021-01-28 03:52:02 +0900

    algorithm_with_python;

---
+++ week_4/02_dfs_recursion.py
@@ -0,0 +1,29 @@
+# 인접 리스트가 주어질때, 모든 노드를 DFS 순서대로 방문하시오.
+
+graph = {
+    1: [2, 5, 9],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3],
+    5: [1, 6, 8],
+    6: [5, 7],
+    7: [6],
+    8: [5],
+    9: [1, 10],
+    10: [9]
+}
+visited = []
+
+
+def dfs_recursion(adjacent_graph, cur_node, visited_array):
+    visited_array.append(cur_node)
+
+    for adjacent_node in adjacent_graph:
+        if adjacent_node not in visited_array:
+            dfs_recursion(adjacent_graph, adjacent_node, visited_array)
+
+    return
+
+
+dfs_recursion(graph, 1, visited)  # 1 이 시작노드
+print(visited)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
